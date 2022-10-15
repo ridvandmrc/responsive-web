1c8701b6511a4693c22d882d25ccfbedccb7d957
Demirci <ridvan.demirci@accenture.com>
2022-10-15 23:42:44 +0300

    added environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { ClientSays, FoodPackages, Header, HealthyEating } from "@components/common";
+import {
+  ClientSays,
+  FoodPackages,
+  Header,
+  HealthyEating,
+} from "@components/common";
 
 function App() {
+  console.log("process: ", process.env);
   return (
     <>
+      {process.env.REACT_APP_ENV}
       <Header />
       <FoodPackages />
       <HealthyEating />
